b5a21944b35efd0e903b890e2c73e3dfdbb2ecaf
Alexander <suulsak@gmail.com>
2023-12-28 16:57:02 +0100

    A

--- examples/advancedespnow/advancedespnow.cpp
+++ examples/advancedespnow/advancedespnow.cpp
@@ -17,8 +17,8 @@ static const String msg = "Hello ESP-NOW!";
 
 static uint8_t receiver[] = { 0x12, 0x34, 0x56, 0x78, 0x90, 0x12 };
 
-#define DEST_ADDR receiver
-//#define DEST_ADDR ESPNOW_BROADCAST_ADDRESS 
+//#define DEST_ADDR receiver
+#define DEST_ADDR ESPNOW_BROADCAST_ADDRESS 
 
 bool sent = true;
 
